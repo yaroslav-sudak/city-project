9113627ee1b73a4ad20fb8894f9265501d9534c2
Burbudol <25privetik2016@gmail.com>
2023-11-27 03:50:18 +0200

    timeline add

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
+// * Slider
+// Slider variables
+let slider = document.getElementById("slider");
+let slideCount = slider.childElementCount;
+let slides = slider.children;
+let currentSlide = 0;
+
+// Slider function
+let changeSlide = (slideNumber) => {
+  slides[slideNumber].classList.remove("slide--down");
+  slides[slideNumber].classList.remove("slide--up");
+  changeActive(slideNumber);
+  for (let i = 0; i < slideCount; i++) {
+    if (i < slideNumber) {
+      slides[i].classList.add("slide--up");
+    } else if (i > slideNumber) {
+      slides[i].classList.add("slide--down");
+    }
+  }
+};
+
+//  * Timeline
+// Timeline variables
+let timelineItems = document.getElementsByClassName("timeline-section__item");
+let timelineActive = document.getElementById("timeline-section__active");
+
+// Timeline function
+let changeActive = (itemNumber) => {
+  timelineActive.style.top = 2 + 45 * itemNumber + "px";
+};
+
+// Timeline Events
+for (let i = 0; i < timelineItems.length; i++) {
+  for (let i = 0; i < timelineItems.length; i++) {
+    timelineItems[i].addEventListener("click", () => {
+      changeActive(i);
+      changeSlide(i);
+    });
+  }
+}
+
 // * Scroll event
 window.addEventListener("wheel", function (event) {
   if (event.deltaY < 0) {
     // Scroll up
+    currentSlide--;
+    if (currentSlide < 0) {
+      currentSlide = slideCount - 1;
+    }
+    changeSlide(currentSlide);
   } else if (event.deltaY > 0) {
     // Scroll down
+    currentSlide++;
+    if (currentSlide >= slideCount) {
+      currentSlide = 0;
+    }
+    changeSlide(currentSlide);
   }
 });
 
@@ -22,8 +73,18 @@ window.addEventListener("touchend", function (e) {
   if (Math.abs(touchEnd - touchStart) > deltaTouch) {
     if (touchStart < touchEnd) {
       // Scroll up
+      currentSlide--;
+      if (currentSlide < 0) {
+        currentSlide = slideCount - 1;
+      }
+      changeSlide(currentSlide);
     } else if (touchStart > touchEnd) {
       // Scroll down
+      currentSlide++;
+      if (currentSlide >= slideCount) {
+        currentSlide = 0;
+      }
+      changeSlide(currentSlide);
     }
   }
 });
